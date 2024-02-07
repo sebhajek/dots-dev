53ea72dc38750701b250f1df11c41c16f8aed7a5
sebhajek <seb.hajek@gmail.com>
2024-02-07 09:21:52 +0100

    scala

---
+++ setup.sh
@@ -0,0 +1,13 @@
+cd ~/.setup-dots/dev;
+
+mkdir -p ~/.setup-dots/dev/jvm;
+wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz -O jvm.tar.gz
+tar -xzf jvm.tar.gz -C ~/.setup-dots/dev/jvm;
+
+curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs;
+chmod +x cs;
+./cs setup;
+sbt --script-version;
+
+cs install giter8;
+cs update g8;
