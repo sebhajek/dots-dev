f8d62c7a3d2f3a407e75d2de65c30f0d9111c140
sebhajek <seb.hajek@gmail.com>
2024-02-07 09:47:31 +0100

    jvm

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 cd ~/.setup-dots/dots/dev;
 
-mkdir -p ~/.setup-dots/dots/dev/jvm;
+mkdir -p ~/.jvm;
 wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz -O jvm.tar.gz
-tar -xzf jvm.tar.gz -C ~/.setup-dots/dots/dev/jvm;
+tar -xzf jvm.tar.gz -C ~/.jvm;
+cd ~/.jvm/jdk-21.0.2+13;
+sudo ln -s ~/.jvm/jdk-21.0.2+13/bin/* /bin;
 
 curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs;
 chmod +x cs;
